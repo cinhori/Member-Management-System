8efbfcca86a23b19cbbeff7657a5bf66e80e1995
cinhori <lleers32@gmail.com>
2016-09-29 10:02:25 +0800

    添加Javabean文件

---
+++ src/tom/bean/Login.java
@@ -0,0 +1,23 @@
+package tom.bean;
+
+public class Login {
+
+	private String name; //»áÔ±Ãû×Ö
+	private String passwd; //ÃÜÂë
+	
+	public String getName() {
+		return name;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	
+	
+}
